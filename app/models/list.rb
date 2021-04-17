02961ce81196aa05b0661ad5db44a13783233a80
Marcelo <marcelo.cansilva@gmail.com>
2021-04-17 14:26:07 -0300

    completed model

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,6 @@
 class List < ApplicationRecord
+  validates_uniqueness_of :name
+  validates :name, presence: true, allow_blank: false
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+  validates_uniqueness_of :title
+  validates :title, :overview, presence: true, allow_blank: false
+  has_many :bookmarks
 end
